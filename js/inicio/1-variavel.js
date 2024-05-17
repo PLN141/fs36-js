867d5c897ff0a06ce2fb599581687429bb33f305
PEDRO LAURINDO NETO <PEDRO0653@DIGITALCOLLEGE.LOCAL>
2024-05-16 21:52:29 -0300

    criando

---
+++ js/inicio/1-variavel.js
@@ -0,0 +1,45 @@
+//variavel pode ser: var, const, let
+
+// 1- Criando a variavel e atribuindo o valor.
+let nom = 'João';
+
+/*
+** Declaração de variável**
+1- não pode ser palavra reservada
+2 - não pode iniciar com número
+3 - não pode ter caracteres especiais
+4 - não pode ter espaço
+5 - não pode ser uma variavel de mesmo nome de outra variavel
+6 - não pode ser uma variavel de mesmo nome de uma função
+7 - não pode ser uma variavel de mesmo nome de um objeto
+8 - não pode ser uma variavel de mesmo nome de um array
+9 - não pode ser uma variavel de mesmo nome de um objeto
+10 - não pode ser uma variavel de mesmo nome de um array
+11- não pode iniciar com operadores aritméticos: +, -, /, *,
+12- não pode iniciar com operadores lógicos: &&, ||, !
+13- não pode iniciar com operadores de atribuição: =, +=, -=, *=, /=, %=, <<=, >>=, >>>=
+14- não pode iniciar com operadores de comparação: ==, !=, ===, !==, <, >, <=, >=
+15- não pode iniciar com operadores de incremento e decremento: ++, --
+
+*/
+let nomee = 'olá';
+let nome5 = 'James';
+let sobre_nome = 'Ferreira'
+// CamelCase: inicia a 1ª palavra em minúsculo e o restante das palavras inicia em maiúscula
+
+// 1- criando/declarando uma variável chamada banana e recebendo o valor 'olá'
+let banana = 'olá';
+// 2- alterando/editando o valor variável era 'olá' para 'Alterei o valor iwrh8q375890p9o'
+banana = 'Alterei o valor iwrh8q375890p9o';
+// 3- alterando/editando o valor variável era 'Alterei o valor iwrh8q375890p9o' para '3000'
+banana = '3000';
+// 4- se não colocar: 'console.log(banana);' será exibido o último valor
+console.log(banana);
+
+// Criando uma constante
+const alturaEvereste = 8848;
+// Atribuindo o valor da variável 
+alturaEvereste = 10500;
+// Imprimindo o valor da variável
+console.log(alturaEvereste)
+// const não pode ser alterado o valor
---
+++ js/inicio/2-tipo_de_dados.js
@@ -0,0 +1,22 @@
+let n1 = 25; 
+let n2 = 15;
+let n3 = 10;
+
+let resultado = n1 + n2 + n3
+
+console.log(resultado)
+
+//soma
+let numero1 = 100;
+let numero2 = 500;
+let palavra = 'palavra'
+//string com sinal de '+' o js entende como concatenação, ou seja, 
+
+let resultados = numero1 + numero2;
+console.log(resultados)
+
+let resultadosMenos= numero1 - numero2;
+console.log(resultadosMenos)
+
+let resultadosPalavra = palavra - numero2;
+console.log(resultadosPalavra)
